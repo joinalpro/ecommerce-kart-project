12bec4bbc17f44076b80d38e1cc9faceb75b0afc
joinalzx <joinalzx@gmail.com>
2025-01-03 09:19:02 +0600

    pagination done

--- store/views.py
+++ store/views.py
@@ -3,9 +3,8 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 
-
 from carts.views import _cart_id
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def store(request, category_slug = None):
@@ -15,14 +14,20 @@ def store(request, category_slug = None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category = categories, is_available = True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
         product_count = products.count()
     else:
         
         products = Product.objects.all().filter(is_available = True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
         product_count = products.count()
     
     context = {
-        'products':products,
+        'products':paged_product,
         'product_count':product_count,
         
     }

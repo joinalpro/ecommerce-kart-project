83852abca506cfe276fc1dfd64b0bc269b9e550a
joinalzx <joinalzx@gmail.com>
2025-01-05 11:38:01 +0600

    product variation

--- carts/views.py
+++ carts/views.py
@@ -42,7 +42,9 @@ def add_cart(request, product_id):
                 cart_item.variations.add(item)
                 
             
-        cart_item.quantity += 1 # cart_item.quantity = cart_item.quantity + 1
+        cart_item.quantity += 1  # cart_item.quantity = cart_item.quantity + 1
+        cart_item.save()
+        
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
             product = product,
